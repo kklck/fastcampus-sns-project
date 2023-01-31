53b4a6f4ec29262f70f32dc65aa86631edf2d68c
kklck <80255826+kklck@users.noreply.github.com>
2023-01-31 21:20:48 +0900

    [commit] sns project fin.

--- src/main/java/com/fastcampus/snsproject/FastcampusSnsProjectApplication.java
+++ src/main/java/com/fastcampus/snsproject/FastcampusSnsProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FastcampusSnsProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FastcampusSnsProjectApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(FastcampusSnsProjectApplication.class, args);
+  }
 
 }
